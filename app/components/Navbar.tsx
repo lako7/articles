6abf85fa04cd45355ad76272e58c4794fdb24518
lako7 <tifeadelako@gmail.com>
2024-09-01 19:13:28 +0100

    header

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,7 +5,7 @@ export default function Navbar() {
     return (
         <nav className="w-full relative flex items-center justify-between max-w-2xl mx-auto px-4 py-5">
             <Link href="/" className="font-bold text-2xl">
-                GenBible<span className="text-primary">AI</span>
+                GenBible<span className="text-teal-600">AI</span>
             </Link>
 
             {/* Button wrapped with Link */}
